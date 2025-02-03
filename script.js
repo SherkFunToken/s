01ba971e3773a34ded3fa5ff4698ae64a8dea948
Sherk Fun Token <sherkfuntoken@gmail.com>
2025-02-03 09:07:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -348,6 +348,16 @@ function initAdminModal() {
   });
 }
 
+  /* FAQ Accordion Logic */
+  const faqItems = document.querySelectorAll('.faq-item');
+  faqItems.forEach(item => {
+    const question = item.querySelector('.faq-question');
+    question.addEventListener('click', () => {
+      // Toggle the "active" class to show or hide the answer
+      item.classList.toggle('active');
+    });
+  });
+
 /* Initialization */
 window.onload = function () {
   setInterval(rotateBanner, 7000);
