bdb17397f8d57752fed32e5d7fb9b21781cfd03a
Sherk Fun Token <sherkfuntoken@gmail.com>
2025-02-03 11:43:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -383,6 +383,35 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   });
+
+
+// Inicialização do Swiper para a seção Team
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 5,
+  spaceBetween: 0,
+  centeredSlides: true,
+  loop: true,
+  simulateTouch: 'ontouchstart' in window || navigator.maxTouchPoints > 0,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// Ajusta a altura dos slides com base na largura para manter a proporção 16:9
+const calculateHeight = () => {
+  const swiperSlideElements = Array.from(document.querySelectorAll('.swiper .swiper-slide'));
+  if (!swiperSlideElements.length) return;
+  const width = swiperSlideElements[0].getBoundingClientRect().width;
+  const height = Math.round(width / (16 / 9));
+  swiperSlideElements.forEach(element => {
+    element.style.height = `${height}px`;
+  });
+};
+
+document.addEventListener("DOMContentLoaded", calculateHeight);
+window.addEventListener('resize', calculateHeight);
+
   
   // Initialization
   setInterval(rotateBanner, 7000);
